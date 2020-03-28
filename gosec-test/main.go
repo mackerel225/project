cd130022944355fce07390b1488ee926bfda5d4b
mackerel225 <davidb9966@gmail.com>
2020-03-28 15:30:37 +0000

    Add files via upload

---
+++ gosec-test/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+func main() {
+	username := "admin"
+	password := "pa34ssw54ord123"
+
+	fmt.Println("Printing hardcoded credentials [1].. ", username, password)
+
+	username2 := "adm"
+	password2 := "psswd"
+
+	fmt.Println("Printing hardcoded credentials [2].. ", username2, password2)
+}
