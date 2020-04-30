ea634708d4df75dd6fecb7d813238ac6f5cfa6d8
mackerel225 <davidb9966@gmail.com>
2020-04-30 22:47:38 +0100

    Update main.go

--- gosec-test/main.go
+++ gosec-test/main.go
@@ -7,5 +7,14 @@ import (
 
 
 func main() {
-	fmt.Println("")
+	/* #nosec */
+	username := "admin"
+	password := "pa34ssw54ord123"
+
+	fmt.Println("Printing hardcoded credentials [1].. ", username, password)
+
+	username2 := "adm"
+	password2 := "psswd"
+
+	fmt.Println("Printing hardcoded credentials [2].. ", username2, password2)
 }
